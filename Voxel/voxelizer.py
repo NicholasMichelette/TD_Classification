ce77825487ca8ddb3dccebfa9fa231085dface53
nmichelette <43892144+nmichelette@users.noreply.github.com>
2021-10-19 23:11:27 -0400

    cleaned up code

--- Voxel/voxelizer.py
+++ Voxel/voxelizer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 import os
+import time
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
@@ -9,7 +10,24 @@ import matplotlib.pyplot as plt
 
 
 def voxelize(points, size): # points must be in the format of a numpy array of arrays, so [[x y z], ...]
-    #max_coord = find_max_coord(points)
+    coords = find_coord_range(points)
+    max_coord = coords[0]
+    min_coord = coords[1]
+    points_norm = np.copy(points)
+
+    for p in range(len(points_norm)):
+        for c in range(len(points_norm[p])):
+            points_norm[p, c] = ((points_norm[p, c] - min_coord[c]) / (max_coord[c]- min_coord[c])) * size
+
+    points_norm = points_norm.astype(int)
+    voxel_grid_tf = np.zeros((size, size, size), dtype=bool)
+    for p in points_norm:
+        voxel_grid_tf[p[0], p[1], p[2]] = True
+    
+    return voxel_grid_tf
+
+
+def voxelize_hit(points, size): # points must be in the format of a numpy array of arrays, so [[x y z], ...]
     coords = find_coord_range(points)
     max_coord = coords[0]
     min_coord = coords[1]
@@ -20,61 +38,11 @@ def voxelize(points, size): # points must be in the format of a numpy array of a
         for c in range(len(points_norm[p])):
             points_norm[p, c] = ((points_norm[p, c] - min_coord[c]) / (max_coord[c]- min_coord[c])) * size
 
-
-    #for p in range(len(points_norm)):
-    #    for c in range(len(points_norm[p])):
-    #        if p < 100:
-    #            print(points_norm[p, c], ' ', points_norm[p], ' ', max_coord[c], ' ', c)
-    #        points_norm[p, c] = ((points_norm[p, c] / max_coord[c] + 1) / 2 ) * size
-            
-    #points_norm = np.true_divide(np.add(np.true_divide(points, max_coord), 1), 2)
-    #points_norm = np.multiply(points_norm, size)
     points_norm = points_norm.astype(int)
     voxel_grid = np.zeros((size, size, size), dtype=int)
-    voxel_grid_tf = np.zeros((size, size, size), dtype=bool)
     for p in points_norm:
         voxel_grid[p[0], p[1], p[2]] += 1
-        voxel_grid_tf[p[0], p[1], p[2]] = True
-    
 
-
-
-    fig = plt.figure()
-    ax = plt.axes(projection = '3d')
-    x = []
-    y = []
-    z = []
-    for i in range(len(voxel_grid)):
-        for j in range(len(voxel_grid[i])):
-            for k in range(len(voxel_grid[i, j])):
-                if voxel_grid[i, j, k] != 0:
-                    x.append(i)
-                    y.append(j)
-                    z.append(k)
-
-    x1 = []
-    y1 = [] 
-    z1 = []
-    #print(len(points))
-    for i in range(len(points)):
-        x1.append(points_norm[i, 0])
-        y1.append(points_norm[i, 1])
-        z1.append(points_norm[i, 2])
-
-    #ax.scatter3D(x, y, z, c=z)
-    #ax.scatter3D(x1, y1, z1, c=z1)
-    ax.grid(True)
-    ax.voxels(voxel_grid_tf)
-    plt.show()
-
-
-def find_max_coord(points):
-    max = np.zeros(3)
-    for p in points:
-        for i in range(len(p)):
-            if abs(p[i]) > max[i]:
-                max[i] = abs(p[i])
-    return np.add(max, 0.000001)
 
 def find_coord_range(points):
     max = np.zeros(3)
@@ -94,6 +62,17 @@ def find_coord_range(points):
 
     return r
 
-#voxelize(pr.read_points('J:/Documents/School/2021 fall/ML2/Project/TD_Classification/data_raw/ModelNet40/bench/train/bench_0001.off'), 32)
-#voxelize(pr.read_points('J:/Documents/School/2021 fall/ML2/Project/TD_Classification/data_raw/ModelNet40/toilet/train/toilet_0322.off'), 32)
-voxelize(pr.read_points('J:/Documents/School/2021 fall/ML2/Project/TD_Classification/data_raw/ModelNet40/airplane/train/airplane_0001.off'), 32)
+
+# For testing purposes
+if __name__ == "__main__":
+    ti = time.time()
+    #voxel_grid_tf = voxelize(pr.read_points('J:/Documents/School/2021 fall/ML2/Project/TD_Classification/data_raw/ModelNet40/bench/train/bench_0001.off'), 32)
+    voxel_grid_tf = voxelize(pr.read_points('J:/Documents/School/2021 fall/ML2/Project/TD_Classification/data_raw/ModelNet40/toilet/train/toilet_0322.off'), 32)
+    #voxel_grid_tf = voxelize(pr.read_points('J:/Documents/School/2021 fall/ML2/Project/TD_Classification/data_raw/ModelNet40/airplane/train/airplane_0001.off'), 32)
+    print(time.time() - ti)
+    fig = plt.figure()
+    ax = plt.axes(projection = '3d')
+    ax.grid(True)
+    ax.voxels(voxel_grid_tf)
+    plt.show()
+    
